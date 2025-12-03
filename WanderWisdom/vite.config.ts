3f23becb221ca8a1200f4dac30be8129936c172b
Adithyan S <adithyan@taxida.in>
2025-12-03 17:12:49 +0530

    feat: setup gh-pages deployment

--- WanderWisdom/vite.config.ts
+++ WanderWisdom/vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
         ]
       : []),
   ],
+  base: 'aari-customer-site',
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
